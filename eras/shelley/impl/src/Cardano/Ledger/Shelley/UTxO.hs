bc859395040abf075f4ca44b4ce8c221d2c4bb66
Max Tomago <noreply@tomagotech.com>
2022-10-26 17:32:11 +0200

    enable UndecidableSuperClasses

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/UTxO.hs
+++ eras/shelley/impl/src/Cardano/Ledger/Shelley/UTxO.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 module Cardano.Ledger.Shelley.UTxO
   ( -- * Primitives
